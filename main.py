1346644568dd4d49f42983760a32de5d6bd23152
Jorghe <x.jorghe.x@gmail.com>
2022-12-14 02:56:29 -0600

    Changed Flask App to Dash App, testing layout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import os
+import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
@@ -7,20 +8,49 @@ import pandas as pd
 import requests
 
 
-app = Flask(__name__)
+app = dash.Dash#Flask(__name__)
 
+stylesheets = ['https://nl-climbing.deta.dev/static/style.css']
 routes = {
     'huaste': 'https://nl-climbing.deta.dev/api/huaste/',
     'salto': 'https://nl-climbing.deta.dev/api/salto/',
     'epc': 'https://nl-climbing.deta.dev/api/epc/'
 }
 
-@app.route('/')
+app.layout = html.Div(children=[
+    html.Header(children=[
+        html.H1('Dash para nlC')
+    ]),
+    html.Main(children=[
+        dcc.Graph(id='graph_huaste')
+    ])
+    
+
+])
+
+""" @app.route('/')
 def index():
     req_huaste = requests.get(routes['huaste'])
     api_huaste = pd.read_json(req_huaste.json())
-    return api_huaste.to_html()
+    return api_huaste.to_json() """
+
+@app.callback(
+    Output('graph_huaste', 'figure')
+)
+def actualizar_grafica():
+    huaste= from_api('huaste')
+    figure = px.scatter(huaste,
+        x = "Area",
+        y = "Grade",
+        hover_data = ['Name', 'Style'],
+
+    )
+    return figure
 
 
+def from_api(name:str):
+    request = requests.get(routes[name])
+    zona = pd.read_json(request.json())
+    return zona
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run_server(debug=True)# , port=os.getenv("PORT", default=5000))
