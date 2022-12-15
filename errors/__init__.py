1f9ae49b9935e97b77bc15103c4115c234be3f4b
Jorghe <x.jorghe.x@gmail.com>
2022-12-15 13:53:09 -0600

    Created errors file to catch 404 and 500 App route render for index changed to alias

---
+++ errors/__init__.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+error_blueprint = Blueprint('errors', __name__)
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ index_blueprint = Blueprint('index',__name__, template_folder='templates', stati
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index')
 
 @app.route("/dash/")
 def my_dash_app():
@@ -33,4 +33,4 @@ def from_api(name:str):
     return zona
 
 if __name__ == '__main__':
-    app.run(debug=True)# , port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
